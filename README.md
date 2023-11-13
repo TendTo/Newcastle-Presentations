9d60340555de8c282e194ae22b07fb417d1fd022
Tend <65033249+TendTo@users.noreply.github.com>
2023-11-13 16:18:19 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# <img src="vendor/img/favicon.svg" align="left" width="70em" height="70em" alt="Icon"> Tutorato Template
+
+<!-- New section -->
+
+## Description
+
+Simple template for the slides of a Tutorato.
+Can easily be extended to support more features and use cases.
+
+<!-- New section -->
+
+## Chapters
+
+<div class="scrollable">
+
+| Chapter                     | Title       |
+| --------------------------- | ----------- |
+| [0](chapters/0-Markdown)    | Markdown    |
+| [1](chapters/1-Quick-start) | Quick-start |
+
+</div>
+
+<!-- New section -->
+
+## Resources
+
+- [reveal.js](https://revealjs.com/)
+- [MathJax](https://www.mathjax.org/)
+- [mermaid](https://mermaid-js.github.io/mermaid/#/)
+
+<!-- New section -->
+
+## Contacts
+
+- [Email](mailto:casablancaernesto@gmail.com)
+- [Telegram personale](https://t.me/TendTo)
+- [Github](https://github.com/TendTo)
---
+++ vendor/js/extension-loader.js
@@ -0,0 +1,118 @@
+import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";
+export { loadMermaid, onRevealReady };
+
+/**
+ * Alter all ids in the svg adding the index of the graph.
+ * Make sure that the style is updated to use the new ids.
+ * @param {string} svgString string of the mermaid svg
+ * @param {number} idx index of the graph. Used to make the ids unique
+ * @returns {string} string of the mermaid svg with updated ids
+ */
+function uniqueIds(svgString, idx) {
+  /** @type {Set<string>} */
+  const set = new Set();
+  // Find al the marker ids in the svg and replace them with `id-${idx}`
+  svgString = svgString.replace(
+    /<marker.*?id=['"]([^"']+)["'].*?<\/marker>/g,
+    (match, id) => {
+      set.add(id);
+      return match.replace(id, `${id}-${idx}`);
+    }
+  );
+  if (set.size === 0) return svgString;
+  // If some ids were found, replace them in the rest of the svg
+  const re = new RegExp(`#(${Array.from(set.values()).join("|")})`, "g");
+  return svgString.replace(re, `#$1-${idx}`);
+}
+
+/**
+ * Find all elements with the class mermaid and ensure that they are rendered
+ * correctly by manually ensuring that the internal ids of the rendered svg
+ * are unique.
+ * If an error occurs during rendering, the error is logged to the console and
+ * the graph is not rendered.
+ */
+function mermaidParser() {
+  const graphs = document.getElementsByClassName("mermaid");
+  graphs.forEach(async (item, index) => {
+    const graphCode = item.textContent.trim();
+    const mermaidDiv = document.createElement("div");
+    mermaidDiv.classList.add("mermaid");
+    mermaidDiv.setAttribute("data-processed", "true");
+
+    try {
+      const { svg } = await mermaid.render(`graph-${index}`, graphCode);
+      mermaidDiv.innerHTML = uniqueIds(svg, index);
+      item.parentNode.parentNode.insertBefore(mermaidDiv, item.parentNode);
+      // If the graph was a fragment, carry it to the rendered svg
+      if (item.parentNode.classList.contains("fragment")) {
+        mermaidDiv.classList.add("fragment");
+      }
+      item.parentNode.remove();
+    } catch (err) {
+      console.error(`Cannot render diagram ${index}\n${graphCode}`);
+      console.error(err.message);
+    }
+  });
+}
+/**
+ * Add support for math block environments in markdown.
+ * The environment is indicated by ```math ... ``` blocks.
+ */
+function mathBlockParser() {
+  const maths = document.getElementsByClassName("math");
+  maths.forEach(async (item) => {
+    const mathDiv = document.createElement("p");
+    const mathCode = item.textContent.trim();
+    mathDiv.classList.add("math");
+    mathDiv.innerText = `$$\n${mathCode}\n$$`;
+
+    item.parentNode.parentNode.insertBefore(mathDiv, item.parentNode);
+    item.parentNode.remove();
+  });
+}
+
+/**
+ * Add support for math dollar environment in markdown.
+ * The environment is indicated by $$ ... $$.
+ */
+function mathDollarParser() {
+  const maths = document.evaluate(
+    '//p[starts-with(., "$$")]',
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+  for (let i = 0; i < maths.snapshotLength; i++) {
+    // Substitute any <em> tags with _ to avoid conflicts with markdown
+    maths.snapshotItem(i).innerHTML = maths
+      .snapshotItem(i)
+      .innerHTML.replace(/<em>/g, "_")
+      .replace(/<\/em>/g, "_");
+
+    const codeEl = document.createElement("code");
+    codeEl.innerHTML = maths.snapshotItem(i).textContent;
+    maths.snapshotItem(i).innerHTML = "";
+    maths.snapshotItem(i).appendChild(codeEl);
+  }
+}
+
+/** Initialize mermaid with custom settings. */
+function loadMermaid() {
+  mermaid.initialize({
+    startOnLoad: false,
+    theme: "dark",
+    flowchart: { htmlLabels: false },
+  });
+}
+
+/** Initialize Reveal with support for personalized extensions. */
+function onRevealReady(event) {
+  mathBlockParser();
+  mermaidParser();
+  mathDollarParser();
+}
+
+window.loadMermaid = loadMermaid;
+Reveal.addEventListener("ready", onRevealReady);
