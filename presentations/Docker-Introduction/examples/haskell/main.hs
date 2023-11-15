4a2b719caa1015001fab903e6959beac62d36963
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-11-15 11:25:01 +0000

    feat: add haskell example

--- presentations/Docker-Introduction/examples/haskell/main.hs
+++ presentations/Docker-Introduction/examples/haskell/main.hs
@@ -2,6 +2,6 @@ module Main where
 
 factorial n = if n == 0 then 1 else n * factorial (n - 1)
 
-main = do putStrLn "What is 5! ?"
+main = do putStrLn "Calculate the factorial of > "
           x <- readLn
-          putStrLn factorial x
+          print $ factorial x
---
+++ presentations/Docker-Introduction/examples/haskell/run.sh
@@ -0,0 +1,2 @@
+docker build -t haskell-example .
+docker run -it --rm haskell-example
